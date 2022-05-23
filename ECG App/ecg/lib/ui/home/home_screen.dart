299f54b51e98556e1d79670509a659b9550ae850
Hannah Lormenyo <lormenyoh@gmail.com>
2022-05-23 21:21:33 +0000

    started home screen

--- ECG App/ecg/lib/ui/home/home_screen.dart
+++ ECG App/ecg/lib/ui/home/home_screen.dart
@@ -12,6 +12,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Stack(
+        children: [
+          Positioned(
+              top: 10,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const <Widget>[Text('Hello Hannah'), CircleAvatar()],
+              ))
+        ],
+      ),
+    ));
   }
 }
