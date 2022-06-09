9f5de53f7953ce8e7cd5278fb469ed1907dd9f93
Hannah Lormenyo <lormenyoh@gmail.com>
2022-06-09 18:26:08 +0000

    added theme

---
+++ ECG App/ecg/lib/config/theme/app_colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const ecgBlue700 = Color(0xFF303E9F);
+
+  static const ecgBlue100 = Color(0XFFEEEFFD);
+
+  static const ecgBlue200 = Color(0xFFE7EAF8);
+
+  static const ecgBlue300 = Color(0XFFD7DCF1);
+
+  static const ecgDarkBlue = Color(0XFF050A30);
+
+  static const ecgYellow100 = Color(0XFFFFF8E0);
+
+  static const ecgGrey100 = Color(0XFF4F5161);
+
+  static const ecgGrey200 = Color(0xFF7B7E95);
+
+  static const ecgPurple = Color(0xFFC85CE3);
+
+  static const ecgWhite = Color(0xFFFFFFFF);
+
+  static const ecgBlack = Color(0xFF000000);
+
+  static const ecgDeepBlueGradient = [
+    Color.fromRGBO(0, 92, 151, 0),
+    Color.fromRGBO(54, 55, 149, 1)
+  ];
+
+  static const ecgLightBlueGradient = [
+    Color.fromRGBO(0, 92, 151, 0.6),
+    Color.fromRGBO(54, 55, 149, 0.6)
+  ];
+}
---
+++ ECG App/ecg/lib/config/theme/app_text_theme.dart
@@ -0,0 +1,86 @@
+import 'package:ecg/config/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+TextTheme ecgLightTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline1: base.headline1!.copyWith(
+          fontSize: 32.0,
+          height: 1.25,
+          fontFamily: 'Satoshi',
+          fontWeight: FontWeight.w300,
+        ),
+        headline2: base.headline2!.copyWith(
+          fontSize: 20.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        headline3: base.headline3!.copyWith(
+          fontSize: 16.0,
+          height: 1.125,
+          fontFamily: 'Satoshi',
+        ),
+        headline4: base.headline4!.copyWith(
+          fontSize: 14.0,
+          height: 1.14,
+          fontFamily: 'Satoshi',
+        ),
+        subtitle1: base.headline3!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        bodyText1: base.bodyText1!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        bodyText2: base.bodyText2!.copyWith(
+          fontSize: 12.0,
+          height: 1.17,
+          fontFamily: 'Satoshi',
+        ),
+        button: base.button!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        caption: base.caption!.copyWith(
+          fontSize: 10.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        overline: base.overline!
+            .copyWith(fontWeight: FontWeight.normal, fontFamily: 'Satoshi'),
+      )
+      .apply(
+        displayColor: AppColors.ecgBlack,
+        bodyColor: AppColors.ecgBlack,
+      );
+}
+
+extension CustomEcgText on TextTheme {
+  TextStyle get ecgTitle900 {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get ecgTitle700 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get ecgTitle500 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Satoshi',
+    );
+  }
+}
