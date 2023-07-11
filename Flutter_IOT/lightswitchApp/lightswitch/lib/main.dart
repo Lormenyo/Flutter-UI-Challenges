761e6d58008a328461c3f90aa1decce1f621af2e
Hannah Lormenyo <lormenyoh@gmail.com>
2023-07-11 23:06:25 +0100

    updated main dart file

--- Flutter_IOT/lightswitchApp/lightswitch/lib/main.dart
+++ Flutter_IOT/lightswitchApp/lightswitch/lib/main.dart
@@ -1,60 +1,45 @@
 // import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:lightswitch/ui/screens/bluetooth_off_screen.dart';
 import 'package:lightswitch/ui/screens/find_devices.dart';
 import 'package:lightswitch/ui/theme/app_theme.dart';
 // import 'package:permission_handler/permission_handler.dart';
 
-import './MainPage.dart';
+void main() {
+  runApp(const LightSwitchApp());
+}
 
-void main() => runApp(const ExampleApplication());
-
-class ExampleApplication extends StatelessWidget {
-  const ExampleApplication({super.key});
+class LightSwitchApp extends StatefulWidget {
+  const LightSwitchApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage());
-  }
+  State<LightSwitchApp> createState() => _LightSwitchAppState();
 }
-// void main() {
-//   if (Platform.isAndroid) {
-//     WidgetsFlutterBinding.ensureInitialized();
-//     [
-//       Permission.location,
-//       Permission.storage,
-//       Permission.bluetooth,
-//       Permission.bluetoothConnect,
-//       Permission.bluetoothScan
-//     ].request().then((status) {
-//       runApp(const LightSwitchApp());
-//     });
-//   } else {
-//     runApp(const LightSwitchApp());
-//   }
-// }
 
-class LightSwitchApp extends StatelessWidget {
-  const LightSwitchApp({Key? key}) : super(key: key);
+class _LightSwitchAppState extends State<LightSwitchApp> {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get current state
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.buildTheme(),
-      home: StreamBuilder<BluetoothState>(
-          stream: FlutterBluePlus.instance.state,
-          initialData: BluetoothState.unknown,
-          builder: (c, snapshot) {
-            final state = snapshot.data;
-            debugPrint('Bluetooth is: ${state == BluetoothState.on}');
-            if (state == BluetoothState.on) {
-              return const FindDevicesScreen();
-            }
-            return BluetoothOffScreen(state: state);
-          }),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.buildTheme(),
+        home: _bluetoothState.isEnabled
+            ? const FindDevicesScreen()
+            : const BluetoothOffScreen());
   }
 }
