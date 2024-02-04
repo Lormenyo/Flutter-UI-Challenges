26a3fc3a6306fb04515ed9d04ad32e7502c63fbf
Hannah Lormenyo <lormenyoh@gmail.com>
2024-02-04 23:37:05 +0000

    added assets, screens and packages

---
+++ amor/lib/ui/screens/rizz_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RizzScreen extends StatefulWidget {
+  const RizzScreen({super.key});
+
+  @override
+  State<RizzScreen> createState() => _RizzScreenState();
+}
+
+class _RizzScreenState extends State<RizzScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
