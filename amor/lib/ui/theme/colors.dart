2a07deb93a229050e62466029d4c6ec028887cfa
Hannah Lormenyo <lormenyoh@gmail.com>
2024-02-04 23:49:58 +0000

    added color theme

---
+++ amor/lib/ui/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color darkBlueColor = const Color(0xFF141F47);
+
+  static Color redColor = const Color(0xFF141F47);
+
+  static Color darkGreyColor = const Color(0xFF848484);
+
+  static Color lightGreyColor = const Color(0xFFD9D9D9);
+}
